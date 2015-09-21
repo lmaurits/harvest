cd79d92d0d4a9962879558f26a573f1476f556b8
Luke Maurits <luke@maurits.id.au>
2015-09-21 15:28:49 +1200

    Use real ISO codes wherever practical for simulated language.  Resort to random 3 alpha character strings only if asked to generate more languages than there are real ISO codes.  Note that there is an implicit limit on how large a simulation can be performed at the moment, but this will be a trivial future fix.

--- harvest/trees.py
+++ harvest/trees.py
@@ -1,11 +1,18 @@
 import itertools
+import random
 
 import dendropy
 from dendropy.simulate import treesim
 
-names = ["".join(chars) for chars in itertools.combinations("abcdefghijklmnopqrstuvwxyz",3)]
+from harvest.true_isos import true_isos
+
+dummy_isos = set(["".join(chars) for chars in itertools.combinations("abcdefghijklmnopqrstuvwxyz",3)])
+dummy_isos = dummy_isos - set(true_isos)
 
 def generate_yule_tree(taxa, birthrate=1.0, taxa_names=None):
-    fancytaxa = dendropy.TaxonNamespace(names[0:taxa])
+    names = random.sample(true_isos, min(taxa, len(true_isos)))
+    if taxa > len(true_isos):
+        names.extend(random.sample(dummy_isos, taxa - len(true_isos)))
+    fancytaxa = dendropy.TaxonNamespace(names)
     tree = treesim.birth_death_tree(birth_rate=birthrate, death_rate=0.0, ntax=taxa, taxon_namespace=fancytaxa)
     return tree
